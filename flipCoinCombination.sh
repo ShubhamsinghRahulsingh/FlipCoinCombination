053f6a8ecc8671e056bcbf5c226107b17a5239b5
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-10-13 23:18:22 +0530

    Added: Triplet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -3,46 +3,71 @@ echo " Welcome to the flip coin combination program"
 
 read -p "Enter the time upto which you want to flip a coin: " n;
 
-HH=0
-HT=0
-TT=0
-TH=0
-
+HHH=0
+HHT=0
+HTH=0
+THH=0
+TTT=0
+TTH=0
+THT=0
+HTT=0
 for (( i=1; i<=n; i++ ))
 do
        flip1=$(($(($RANDOM%10))%2))
        flip2=$(($(($RANDOM%10))%2))
-       if [ $flip1 -eq 1 -a $flip2 -eq 1 ]
+       flip3=$(($(($RANDOM%10))%2))
+       if [ $flip1 -eq 1 -a $flip2 -eq 1 -a $flip3 -eq 1 ]
        then
-            HH=$(($HH+1))
+            HHH=$(($HHH+1))
 
-       elif [ $flip1 -eq 1 -a $flip2 -eq 0 ]
+       elif [ $flip1 -eq 1 -a $flip2 -eq 1 -a $flip3 -eq 0 ]
        then
-            HT=$(($HH+1))
-       elif [ $flip1 -eq 0 -a $flip2 -eq 0 ]
+            HHT=$(($HHT+1))
+       elif [ $flip1 -eq 1 -a $flip2 -eq 0 -a $flip3 -eq 1 ]
        then
-            TT=$(($TT+1))
+            HTH=$(($HTH+1))
+       elif [ $flip1 -eq 0 -a $flip2 -eq 1 -a $flip3 -eq 1 ]
+       then
+            THH=$(($THH+1))
+       elif [ $flip1 -eq 0 -a $flip2 -eq 0 -a $flip3 -eq 0 ]
+       then
+            TTT=$(($TTT+1))
+       elif [ $flip1 -eq 0 -a $flip2 -eq 0 -a $flip3 -eq 1 ]
+       then
+            TTH=$(($TTH+1))
+       elif [ $flip1 -eq 0 -a $flip2 -eq 1 -a $flip3 -eq 0 ]
+       then
+            THT=$(($THT+1))
        else
-            TH=$(($TH+1))
+            HTT=$(($HTT+1))
        fi
 done
 declare -A flip
-flip[hh]=$HH
-flip[ht]=$HT
-flip[tt]=$TT
-flip[th]=$TH
-echo "Head in both comes ${flip[hh]} times"
-echo "Head in first and Tail in second comes ${flip[ht]} times"
-echo "Tail in both comes ${flip[tt]} times"
-echo "Tail in first and Head in second comes ${flip[th]} times"
+flip[hhh]=$HHH
+flip[hht]=$HHT
+flip[hth]=$HTH
+flip[thh]=$THH
+flip[ttt]=$TTT
+flip[tth]=$TTH
+flip[tht]=$THT
+flip[htt]=$HTT
+echo "we get:${flip[@]}
 
-total=$((${flip[hh]}+${flip[ht]}+${flip[tt]}+${flip[th]}))
+total=$((${flip[hhh]}+${flip[hht]}+${flip[hth]}+${flip[thh]}+${flip[ttt]}+${flip[tth]}+${flip[tht]}+${flip[htt]}))
 
-HHPercentage=`awk 'BEGIN{printf("%0.2f",'${flip[hh]}/$total' * '100')}'`
-HTPercentage=`awk 'BEGIN{printf("%0.2f",'${flip[ht]}/$total' * '100')}'`
-TTPercentage=`awk 'BEGIN{printf("%0.2f",'${flip[tt]}/$total' * '100')}'`
-THPercentage=`awk 'BEGIN{printf("%0.2f",'${flip[th]}/$total' * '100')}'`
-echo "Both Head percentage is $HHPercentage"
-echo "Head and Tail percentage is $HTPercentage"
-echo "Both Tail percentage is $TTPercentage"
-echo "Tail and Head percentage is $THPercentage"
+HHHPercentage=`awk 'BEGIN{printf("%0.2f",'${flip[hhh]}/$total' * '100')}'`
+HHTPercentage=`awk 'BEGIN{printf("%0.2f",'${flip[hht]}/$total' * '100')}'`
+HTHPercentage=`awk 'BEGIN{printf("%0.2f",'${flip[hth]}/$total' * '100')}'`
+THHPercentage=`awk 'BEGIN{printf("%0.2f",'${flip[thh]}/$total' * '100')}'`
+TTTPercentage=`awk 'BEGIN{printf("%0.2f",'${flip[ttt]}/$total' * '100')}'`
+TTHPercentage=`awk 'BEGIN{printf("%0.2f",'${flip[tth]}/$total' * '100')}'`
+THTPercentage=`awk 'BEGIN{printf("%0.2f",'${flip[tht]}/$total' * '100')}'`
+HTTPercentage=`awk 'BEGIN{printf("%0.2f",'${flip[htt]}/$total' * '100')}'`
+echo "HHHPercentage:$HHHPercentage"
+echo "HHTPercentage:$HHTPercentage"
+echo "HTHPercentage:$HTHPercentage"
+echo "THHPercentage:$THHPercentage"
+echo "TTTPercentage:$TTTPercentage"
+echo "TTHPercentage:$TTHPercentage"
+echo "THTPercentage:$THTPercentage"
+echo "HTTPercentage:$HTTPercentage"
